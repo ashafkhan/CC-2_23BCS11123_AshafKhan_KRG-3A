7ced1d7adb79fa86b14f983517fe345367f127cb
Ashaf Khan <khanashaf02@gmail.com>
2026-02-26 12:41:07 +0530

    Added chapter-6 content

---
+++ L06_Pattern_Match/kmp.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(NULL);
+    cin.tie(NULL);
+
+    string text, pattern;
+    getline(cin, text);
+    getline(cin, pattern);
+
+    
+}
