2bb3dca9353a5fa4613a03f72f300e295122bc85
Ashaf Khan <khanashaf02@gmail.com>
2026-04-28 15:00:20 +0530

    Add experiment-1 code

--- Experiment-1/exp1.cpp
+++ Experiment-1/exp1.cpp
@@ -1,38 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
-public:
-    static const int MOD = 1e9 + 7;
+// Compute C(n, k) % 10 using iterative method
+int nCr_mod10(int n, int r) {
+    if (r > n) return 0;
+    if (r > n - r) r = n - r;
 
-    long long gcd(long long a, long long b) {
-        while (b != 0) {
-            long long t = a % b;
-            a = b;
-            b = t;
-        }
-        return a;
+    long long res = 1;
+    for (int i = 0; i < r; i++) {
+        res = res * (n - i);
+        res /= (i + 1);
+        res %= 10;  // keep mod 10
+    }
+    return res % 10;
+}
+
+int triangularSum(vector<int>& nums) {
+    int n = nums.size();
+    int ans = 0;
+
+    for (int i = 0; i < n; i++) {
+        int coeff = nCr_mod10(n - 1, i);
+        ans = (ans + nums[i] * coeff) % 10;
     }
 
-    long long lcm(long long a, long long b) {
-        return (a / gcd(a, b)) * b;
-    }
+    return ans;
+}
 
-    int nthMagicalNumber(int n, int a, int b) {
-        long long low = 1;
-        long long high = (long long)n * min(a, b);
-        long long L = lcm(a, b);
-
-        while (low < high) {
-            long long mid = low + (high - low) / 2;
-            long long cnt = mid / a + mid / b - mid / L;
-
-            if (cnt < n)
-                low = mid + 1;
-            else
-                high = mid;
-        }
-
-        return low % MOD;
-    }
-};
+int main() {
+    vector<int> nums = {1, 2, 3, 4, 5};
+    cout << triangularSum(nums) << endl;  // Output: 8
+    return 0;
+}
